dee5ba6f9a4c910f4e2ed65c92fd23d8e2f01880
Faraz Shaikh <farazzshaikh@gmail.com>
2025-06-28 19:53:44 +0400

    Update demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Lights from "./components/Lights";
 import { RaymarchedScene } from "./Raymarching/RaymarchedScene";
-import "./styles.css";
 
 export default function App() {
   return (
